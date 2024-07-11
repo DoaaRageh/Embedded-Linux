4e401d7e4b4df4774009ad2632bdc61c5f8d04b0
DoaaRageh <doaaelryan976@gmail.com>
2024-07-11 22:50:21 +0200

    Bash Task

---
+++ Linux_Admin/Bash_Task/README.md
@@ -0,0 +1,11 @@
+# Objective
+Create a Bash script that organizes files from a source directory into subdirectories
+based on their file types.
+
+# Instructions
+## Setup:
+**Download the Script:** Save the script as file_organizer.sh in your desired location.
+## Execute the Script:
+**To run the script, use the following command in the terminal:**``bash file_organizer.sh "./Source"``
+"./Source" is the path to the directory containing the files you want to organize.
+
---
+++ Linux_Admin/Bash_Task/file_organizer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+directory=$1
+mkdir -p "$directory/images" "$directory/documents" "$directory/others"
+files=$(ls "$directory")
+for file in $files;do
+	if [ -f "$directory/$file" ];then
+		if [[ "$file" == *.jpg || "$file" == *.png || "$file" == *.gif ]];then
+			mv "$directory/$file" "$directory/images/$file"	
+		elif [[ $file == *.txt || $file == *.doc || $file == *.pdf ]];then			mv "$directory/$file" "$directory/documents/$file"
+		else
+			mv "$directory/$file" "$directory/others/$file"
+		fi
+	fi
+done
